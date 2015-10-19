cdcf8f76ddc7f55ba47a1b0fcc7db822db8ccea9
Gabriella Castaneda <gabriellacastaneda@gmail.com>
2015-10-19 23:44:03 +0000

    validating new entries

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true #presents new place from being saved without a name
+	validates :name, length: {minimum:3} , :presence => true
+	validates :address, :presence => true #presents new place from being saved without a name
+	validates :description, :presence => true
 end

5719af19489d2e85fbf606d9ea70d035d9d3b469
Gabriella Castaneda <gabriellacastaneda@gmail.com>
2015-10-23 21:48:53 +0000

    email link

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,11 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@burp.com"
 
-  def comment_added
-  	mail(to: "gabriellacastaneda@gmail.com",
-  		subject: "A comment has been added to your brewery!")
+  def comment_added(comment)
+  	@place = comment.place
+  	@place_owner = @place.user
+  	mail(to: @place_owner.email,
+  		subject: "A comment has been added to #{@place.name}!")
   end
 
 end

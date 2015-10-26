a30f79f309359680ca68963a067021c7f4cacca7
Gabriella Castaneda <gabriellacastaneda@gmail.com>
2015-10-26 17:09:02 +0000

    user comment counts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
 	def show
+		@user = User.find(params[:id]) #takes id from url, looks the user up
+		# then puts that user in the @user value which we can use inside the view
 	end
 end
